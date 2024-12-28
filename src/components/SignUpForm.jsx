06898bf56e5cabf6ad1ae5b9c7dcaf9b0094c401
Bianca Santos <biancassantos89@gmail.com>
2024-12-28 14:56:38 -0300

    react hook form added

--- src/components/SignUpForm.jsx
+++ src/components/SignUpForm.jsx
@@ -1,22 +1,39 @@
+import { useForm } from "react-hook-form";
 
 function SignUpForm() {
+  const { 
+    register, 
+    handleSubmit, 
+    formState: {
+      errors, 
+      isSubmitting
+    }} = useForm();
+
+  function onSubmit() {
+    //
+  }
+
   return (
     <form
+    onSubmit={handleSubmit(onSubmit)}
     className="flex flex-col gap-[8px] w-full">
       <input 
       type="email" 
-      name="email" 
-      id="email" 
+      {...register("email", {required: "Por favor, informe um e-mail."})}
       placeholder="email@exemplo.com"
       className="rounded border-[1px] border-gray-300 p-1" />
+      {errors.email && <p className="text-xs text-red-600">{errors.email.message}</p>}
+
       <input 
       type="password" 
-      name="password" 
-      id="password" 
+      {...register("password", {required: "Por favor, informe uma senha"})} 
       placeholder="Senha"
       className="rounded border-[1px] border-gray-300 p-1" />
+      {errors.password && <p className="text-xs text-red-600">{errors.password.message}</p>}
+
       <button 
       type="submit"
+      disabled={isSubmitting}
       className="rounded bg-emerald-500 hover:bg-emerald-600 duration-300 p-1 text-white text-sm" >
         Criar uma nova conta
       </button>
