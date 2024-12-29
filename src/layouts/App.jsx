be76cab24e00a100c129f0150b2220cd24360d63
Bianca Santos <biancassantos89@gmail.com>
2024-12-29 12:52:59 -0300

    props validation fixed

--- src/layouts/App.jsx
+++ src/layouts/App.jsx
@@ -1,13 +1,7 @@
-import PropTypes from "prop-types";
-
 function App({ children }) {
   return (
     <>{children}</>
   )
-}
-
-App.propTypes = {
-  children: PropTypes.node
 }
 
 export default App;
