06eb505f9e6b8c7fe1c1854a5dd5ca079f8e455e
Bianca Santos <biancassantos89@gmail.com>
2025-01-01 15:00:30 -0300

    firebase initial config

---
+++ src/config/firebase.js
@@ -0,0 +1,13 @@
+import { initializeApp } from 'firebase/app';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyChtQoeK5FwVE7sSlv6wyjc9wre8HXfLY8",
+  authDomain: "aluritter-a34b9.firebaseapp.com",
+  projectId: "aluritter-a34b9",
+  storageBucket: "aluritter-a34b9.firebasestorage.app",
+  messagingSenderId: "603017297986",
+  appId: "1:603017297986:web:1ac167f742fb375255addb",
+  measurementId: "G-QN5GW46559"
+};
+
+const app = initializeApp(firebaseConfig);
