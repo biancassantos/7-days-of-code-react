8b62bf1558762b7699afb1a0505350596098a077
Bianca Santos <biancassantos89@gmail.com>
2025-01-03 09:51:32 -0300

    firebase info fixed

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -3,12 +3,12 @@ import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
   apiKey: "AIzaSyChtQoeK5FwVE7sSlv6wyjc9wre8HXfLY8",
-  authDomain: "aluritter-a34b9.firebaseapp.com",
-  projectId: "aluritter-a34b9",
-  storageBucket: "aluritter-a34b9.firebasestorage.app",
-  messagingSenderId: "603017297986",
-  appId: "1:603017297986:web:1ac167f742fb375255addb",
-  measurementId: "G-QN5GW46559"
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID,
+  measurementId: import.meta.env.VITE_MEASUREMENT_ID
 };
 
 const app = initializeApp(firebaseConfig);
